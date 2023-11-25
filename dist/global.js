4373a9d70e12f7866cbbd70822b5992bd8aacf1f
jmjjmj994 <jonathan.mjos@outlook.com>
2023-11-25 13:21:28 +0100

    Added code for displaying different page type based on token is valid

--- dist/global.js
+++ dist/global.js
@@ -1,13 +1,33 @@
-import { localStorageItems } from "./js/utils/utils.js";
+/* import { localStorageItems } from "./js/utils/utils.js";
+console.log(localStorageItems)
 const userData = { ...localStorageItems };
 const avatar = document.querySelectorAll("[data-avatar]");
 const credit = document.querySelectorAll("[data-credit]");
 const signUp = document.querySelector("[data-signup]")
 const loginLogout = document.querySelector("[data-login-logout]");
-const userStatus = () => {
 
-    if (userData.token) {
+
+
+const userStatus = () => {
+    if (!userData.token && window.location.pathname === "/login.html") {
+        loginLogout.textContent = "Logg inn";
+        signUp.textContent = "Registrer"
+    }
+
+    if (window.location.pathname === "/signup.html") {
+        loginLogout.textContent = "Logg inn";
+        signUp.textContent = "Registrer"
+        avatar.forEach(img => {
+            userData.userData.avatar ? img.src = userData.userData.avatar : img.src = "dist/assets/blank-profile-picture-973460_640.png"
+        })
+    }
+
+    if (userData.token && window.location.pathname === "/index.html") {
         loginLogout.textContent = "Logg ut"
+        loginLogout.addEventListener("click", (e) => {
+        localStorage.removeItem("user-token");
+      
+        })
         avatar.forEach(img => {
             userData.userData.avatar ? img.src = userData.userData.avatar : img.src = "dist/assets/blank-profile-picture-973460_640.png"
         })
@@ -19,41 +39,34 @@ const userStatus = () => {
     } else {
         avatar.forEach(img => {
             img.src = "dist/assets/blank-profile-picture-973460_640.png"
-
         })
-        avatar.alt = "";
-        credit.textContent = "";
-        loginLogout.textContent = "Logg inn"
-        signUp.textContent ="Registrer"
-
+        loginLogout.textContent = "Logg inn";
+        signUp.textContent = "Registrer"
     }
 
-}
-
-const logOut = () => {
-loginLogout.addEventListener("click", (e) => {
-localStorage.removeItem("user-token");
-
-})
 
 }
-logOut()
+
+
+const tokenPresent = () => {
+
+
+    const hasCheckedToken = localStorage.getItem('tokenChecked');
+console.log(hasCheckedToken)
+    console.log("Hei")
+
+
+};
+
+
+document.addEventListener('DOMContentLoaded', tokenPresent);
 userStatus()
+ */
 
 
-
-
-
-if (window.location.pathname === "/signup.html") {
-    console.log("URL pathname matches '/login.html'");
-    console.log(window.location.pathname);
-} else {
-    console.log("URL pathname does not match '/login.html'");
-    console.log("false");
-}
-/* 
-const isLoggedIn = () => {
-    return !!userdata.token
-}
-*/
-
+/*    if (!hasCheckedToken && userData.token) {
+       localStorage.setItem('tokenChecked', 'true');
+       window.location.href = "/index.html";
+   } else if (!userData.token && hasCheckedToken !== 'true') {
+       localStorage.removeItem('tokenChecked'); 
+   } */
--- dist/js/utils/utils.js
+++ dist/js/utils/utils.js
@@ -1,5 +1,6 @@
 export const localStorageItems = {
 userData: JSON.parse(localStorage.getItem("user-data")),
-token : localStorage.getItem("user-token")
+token : localStorage.getItem("user-token"),
+hasToken : localStorage.getItem("tokenChecked")
 
 }
