d3f095063d2da0c136439a00626fa64ce03617ca
jmjjmj994 <jonathan.mjos@outlook.com>
2023-12-08 14:31:23 +0100

    Pagination when user searches for tags is finished. Handled edge cases appropriately

--- src/js/pages/bidding.js
+++ src/js/pages/bidding.js
@@ -1,24 +1,23 @@
-import { createCardElement, createButtonElement, dateConverter } from "../utils/utils.js";
-
-
+import { createCardElement, norwegianEndDate } from "../utils/utils.js"
+const auctionContainer = document.querySelector("[data-type-section='listings']")
+const buttonContainer = document.querySelector("[data-type-section='pagination-buttons']")
 
 const params = new URLSearchParams(window.location.search);
-console.log(params)
 const currentPage = params.get("page")
+const currentResult = params.get("results")
 let getPageNum;
 
-if(!isNaN(parseInt(currentPage))) {
-getPageNum = parseInt(currentPage);
+if (!isNaN(parseInt(currentPage))) {
+    getPageNum = parseInt(currentPage);
 
 } else {
     getPageNum = 0;
 }
 
 
-
-const fetchListings = async () => {
+async function fetchListings(tag) {
     const url =
-        `https://api.noroff.dev/api/v1/auction/listings?_seller=true&_active=true&limit=10&offset=${getPageNum * 10}`;
+        `https://api.noroff.dev/api/v1/auction/listings?_seller=true&_active=true&&_bids=true&_tag=${tag}&limit=10&offset=${getPageNum * 10}`;
     try {
         const res = await fetch(url, {
             method: "GET",
@@ -28,7 +27,7 @@ const fetchListings = async () => {
         });
 
         if (res.ok) {
-            const data = await res.json();
+            const data = await res.json()
             return data;
 
         } else {
@@ -45,84 +44,133 @@ const fetchListings = async () => {
 
 
 
+const initializer = async () => {
+    const nextButton = document.querySelector("[data-type-section='pagination-next-btn']");
+    const prevButton = document.querySelector("[data-type-section='pagination-prev-btn']");
+    if (currentResult) {
+        const resultsValue = currentResult.substring(" ");
+        nextButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?results=${resultsValue}&page=${getPageNum + 1}`;
+        });
+        prevButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?results=${resultsValue}&page=${getPageNum - 1}`;
+        });
+        const data = await fetchListings(resultsValue);
+        handleData(data);
+    } else {
+        nextButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?page=${getPageNum + 1}`;
+        });
+        prevButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?page=${getPageNum - 1}`;
+        });
+        const data = await fetchListings("");
+        handleData(data);
+    }
+};
 
-const listeners = async (end) => {
-    console.log(end)
-    const buttonContainer = document.querySelector("[data-type-section='pagination-buttons']")
+const handleData = (data) => {
     const nextButton = document.querySelector("[data-type-section='pagination-next-btn']");
     const prevButton = document.querySelector("[data-type-section='pagination-prev-btn']");
 
- /*    const nextButton = createCardElement("a", "bg-blue p-2");
-    const prevButton = createCardElement("a", "bg-red p-2"); */
 
-    if(getPageNum === 0) {
-        prevButton.style.display ="none"
-    }
-
-    if(end) {
-        prevButton.style.display ="none"
-        nextButton.style.display ="none"
-    }
-    nextButton.addEventListener("click",async (e) => {
-        e.target.href = `/bidding.html?page=${getPageNum + 1}`
-    })
-
-    prevButton.addEventListener("click", async (e) => {
-        e.target.href = `/bidding.html?page=${getPageNum - 1}`
-    })
-
-}
-
-
-
-
- function createCards(media) {
-        const article = createCardElement("article")
-    const articleHeader = createCardElement("div", "relative h-full w-full")
-    const articleImage = createCardElement("img")
-    articleImage.src = media;
-    articleImage.loading = "lazy"
-    articleHeader.append(articleImage)
-    const articleBody = createCardElement("div")
-    const articleFooter = createCardElement("div")
-    article.append(articleHeader)
-    return article
-}
-
-
-const renderListings =  async () =>  {
-    let end = false;
-    const listingsContainer = document.querySelector("[data-type-section='listings']")
-    const listingData = await fetchListings() 
-    console.log(listingData)
-    listingsContainer.innerHTML = "";
-
-    if(listingData.length === 0) {
-        listingsContainer.innerHTML = "No listings"
-        end = true;
-        listeners(end)
-
+    if (data.length < 10) {
+        nextButton.classList.add('hidden');
+        buttonContainer.classList.add('hidden');
     } else {
-        listingData.forEach(item => {
-            const { media } = item;
-            const cards = createCards(media)
-            listingsContainer.append(cards)
-
-        }) 
+        prevButton.classList.remove('hidden');
+        nextButton.classList.remove('hidden');
     }
-  
+
+    if (data.length === 0) {
+        prevButton.classList.add('hidden');
+        nextButton.classList.add('hidden');
+        renderAuctionCards("No more data");
+    }
+
+    if (getPageNum === 0) {
+        prevButton.classList.add('hidden');
+    }
+    renderAuctionCards(data)
+};
+
+
+
+
+
+
+
+const auctionCards = (title, image, bids, date, id) => {
+    console.log(title)
+    const createBidArray = bids.map(bid => bid.amount)
+    const highestBid = Math.max(...createBidArray);
+    const totalBids = createBidArray.reduce((accumulator, bidObj) => accumulator + bidObj)
+    const article = createCardElement("article", "flex flex-col  relative  bg-custom-card relative shadow-md text-custom-textGrey");
+    const articleLink = createCardElement("a", "absolute h-full w-full custom-z-low");
+    articleLink.href = `/specific.html?id=${id}`
+    const articleHeader = createCardElement("div", " h-[70%] relative")
+    const articleHeaderImage = createCardElement("img", "absolute object-cover w-full h-full ");
+    image.length === 0 ? articleHeaderImage.src = "src/assets/no-image.jpg" : articleHeaderImage.src = image;
+    articleHeaderImage.alt = "Auksjons-produkt";
+    articleHeader.append(articleHeaderImage)
+    const articleBody = createCardElement("div", "basis-[auto] flex   p-1");
+    const articleBodyTitle = createCardElement("span", "card-title-typography mt-6");
+    articleBodyTitle.textContent = title;
+    articleBody.append(articleBodyTitle)
+    const articleFooter = createCardElement("div", "flex basis-[auto] py-3 justify-between items-end p-1 ");
+    const articleFooterCol1 = createCardElement("div", "flex flex-col ")
+    const articleFooterCol1TotalBids = createCardElement("span", "mb-3")
+    articleFooterCol1TotalBids.textContent = `Totale bud:${totalBids}`
+    const articleFooterCol1HighestBid = createCardElement("span", "")
+    articleFooterCol1HighestBid.textContent = `Høyeste bud:${highestBid}`
+    articleFooterCol1.append(articleFooterCol1TotalBids, articleFooterCol1HighestBid)
+    const articleFooterCol2 = createCardElement("div", "");
+    const articleFooterCol2Icon = createCardElement("i", "fa-regular fa-clock")
+    const articleFooterCol2EndDate = createCardElement("span")
+    articleFooterCol2EndDate.textContent = date
+    articleFooterCol2.append(articleFooterCol2Icon, articleFooterCol2EndDate)
+    articleFooter.append(articleFooterCol1, articleFooterCol2)
+    article.append(articleLink)
+    article.append(articleHeader, articleBody, articleFooter)
+    return article;
 }
- 
+
+
+const renderAuctionCards = async (data) => {
+    try {
+        if (typeof data === "string") {
+            auctionContainer.innerHTML = "No data available.";
+
+        } else {
+            const articleCard = auctionCards;
+            auctionContainer.innerHTML = "";
+            const auctionData = data.filter(item =>
+                item.title && item.media && item._count && item._count.bids &&
+                item.bids && Array.isArray(item.bids)
+            ).map(({ title, media, _count, endsAt, id, bids }) => ({
+                title,
+                media,
+                bids,
+                endsAt,
+                id
+            }));
+
+            if (auctionData.length > 0) {
+                auctionData.forEach(item => {
+                    const norwegianTime = norwegianEndDate(item.endsAt);
+                    auctionContainer.append(articleCard(item.title, item.media, item.bids, norwegianTime, item.id));
+                });
+            } else {
+                auctionContainer.innerHTML = "No data available.";
+                buttonContainer.display = "none"
+
+            }
+        }
+    } finally {
+
+    }
+};
 
 
 
-
-
-
-
-const initializer = ()  => {
-    fetchListings()
-    listeners()
-    renderListings()
-}
-initializer()
+initializer();
--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-import { createCardElement } from "./src/js/utils/utils.js";
-
+import { createCardElement, norwegianEndDate } from "./src/js/utils/utils.js";
+const auctionContainer = document.querySelector("[data-type-section='listings']")
+const buttonContainer = document.querySelector("[data-type-section='pagination-buttons']")
+console.log(buttonContainer)
+console.log(auctionContainer)
 const params = new URLSearchParams(window.location.search);
 const currentPage = params.get("page")
 const currentResult = params.get("results")
@@ -15,7 +18,7 @@ if (!isNaN(parseInt(currentPage))) {
 
 async function fetchListings(tag) {
     const url =
-        `https://api.noroff.dev/api/v1/auction/listings?_seller=true&_active=true&_tag=${tag}&limit=10&offset=${getPageNum * 10}`;
+        `https://api.noroff.dev/api/v1/auction/listings?_seller=true&_active=true&&_bids=true&_tag=${tag}&limit=10&offset=${getPageNum * 10}`;
     try {
         const res = await fetch(url, {
             method: "GET",
@@ -26,13 +29,7 @@ async function fetchListings(tag) {
 
         if (res.ok) {
             const data = await res.json()
-
-            if (data.length > 0) {
-                console.log(data)
-                return data;
-            } else {
-                return "no more data";
-            }
+            return data;
 
         } else {
             throw new Error("Failed to fetch data")
@@ -47,137 +44,137 @@ async function fetchListings(tag) {
 
 
 
+
 const initializer = async () => {
     const nextButton = document.querySelector("[data-type-section='pagination-next-btn']");
     const prevButton = document.querySelector("[data-type-section='pagination-prev-btn']");
-    if (getPageNum === 0) {
-        prevButton.style.display = "none"
-    }
     if (currentResult) {
         const resultsValue = currentResult.substring(" ");
-        nextButton.addEventListener("click", async (e) => {
-            e.target.href = `/bidding.html?results=${resultsValue}&page=${getPageNum + 1}`
-        })
-        prevButton.addEventListener("click", async (e) => {
-            e.target.href = `/bidding.html?results=${resultsValue}&page=${getPageNum - 1}`
-        })
+        nextButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?results=${resultsValue}&page=${getPageNum + 1}`;
+        });
+        prevButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?results=${resultsValue}&page=${getPageNum - 1}`;
+        });
+        const data = await fetchListings(resultsValue);
+        handleData(data);
     } else {
-        nextButton.addEventListener("click", async (e) => {
-            e.target.href = `/bidding.html?page=${getPageNum + 1}`
-        })
-
-        prevButton.addEventListener("click", async (e) => {
-            e.target.href = `/bidding.html?page=${getPageNum - 1}`
-        })
-
+        nextButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?page=${getPageNum + 1}`;
+        });
+        prevButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?page=${getPageNum - 1}`;
+        });
+        const data = await fetchListings("");
+        handleData(data);
     }
+};
 
-  
-
-
-
-}
-
-
-if (currentResult) {
-    const resultsValue = currentResult.substring(" ");
-    fetchListings(resultsValue)
-} else if (currentPage) {
-    fetchListings("")
-} else {
-    fetchListings("")
-}
-
-
-
-initializer()
-
-
-
-
-
-
-/* 
-
-
-
-
-const listeners = async (end) => {
-    console.log(end)
-    const buttonContainer = document.querySelector("[data-type-section='pagination-buttons']")
+const handleData = (data) => {
     const nextButton = document.querySelector("[data-type-section='pagination-next-btn']");
     const prevButton = document.querySelector("[data-type-section='pagination-prev-btn']");
 
 
+    if (data.length < 10) {
+        nextButton.classList.add('hidden');
+        buttonContainer.classList.add('hidden');
+    } else {
+        prevButton.classList.remove('hidden');
+        nextButton.classList.remove('hidden');
+    }
+
+    if (data.length === 0) {
+        prevButton.classList.add('hidden');
+        nextButton.classList.add('hidden');
+        renderAuctionCards("No more data");
+    }
 
     if (getPageNum === 0) {
-        prevButton.style.display = "none"
+        prevButton.classList.add('hidden');
     }
+    renderAuctionCards(data)
+};
 
-    if (end) {
-        prevButton.style.display = "none"
-        nextButton.style.display = "none"
+
+
+
+
+
+
+const auctionCards = (title, image, bids, date, id) => {
+    console.log(title)
+    const createBidArray = bids.map(bid => bid.amount)
+    const highestBid = Math.max(...createBidArray);
+    const totalBids = createBidArray.reduce((accumulator, bidObj) => accumulator + bidObj)
+    const article = createCardElement("article", "flex flex-col  relative  bg-custom-card relative shadow-md text-custom-textGrey");
+    const articleLink = createCardElement("a", "absolute h-full w-full custom-z-low");
+    articleLink.href = `/specific.html?id=${id}`
+    const articleHeader = createCardElement("div", " h-[70%] relative")
+    const articleHeaderImage = createCardElement("img", "absolute object-cover w-full h-full ");
+    image.length === 0 ? articleHeaderImage.src = "src/assets/no-image.jpg" : articleHeaderImage.src = image;
+    articleHeaderImage.alt = "Auksjons-produkt";
+    articleHeader.append(articleHeaderImage)
+    const articleBody = createCardElement("div", "basis-[auto] flex   p-1");
+    const articleBodyTitle = createCardElement("span", "card-title-typography mt-6");
+    articleBodyTitle.textContent = title;
+    articleBody.append(articleBodyTitle)
+    const articleFooter = createCardElement("div", "flex basis-[auto] py-3 justify-between items-end p-1 ");
+    const articleFooterCol1 = createCardElement("div", "flex flex-col ")
+    const articleFooterCol1TotalBids = createCardElement("span", "mb-3")
+    articleFooterCol1TotalBids.textContent = `Totale bud:${totalBids}`
+    const articleFooterCol1HighestBid = createCardElement("span", "")
+    articleFooterCol1HighestBid.textContent = `Høyeste bud:${highestBid}`
+    articleFooterCol1.append(articleFooterCol1TotalBids, articleFooterCol1HighestBid)
+    const articleFooterCol2 = createCardElement("div", "");
+    const articleFooterCol2Icon = createCardElement("i", "fa-regular fa-clock")
+    const articleFooterCol2EndDate = createCardElement("span")
+    articleFooterCol2EndDate.textContent = date
+    articleFooterCol2.append(articleFooterCol2Icon, articleFooterCol2EndDate)
+    articleFooter.append(articleFooterCol1, articleFooterCol2)
+    article.append(articleLink)
+    article.append(articleHeader, articleBody, articleFooter)
+    return article;
+}
+
+
+const renderAuctionCards = async (data) => {
+    try {
+        if (typeof data === "string") {
+            auctionContainer.innerHTML = "No data available.";
+
+        } else {
+            const articleCard = auctionCards;
+            auctionContainer.innerHTML = "";
+            const auctionData = data.filter(item =>
+                item.title && item.media && item._count && item._count.bids &&
+                item.bids && Array.isArray(item.bids)
+            ).map(({ title, media, _count, endsAt, id, bids }) => ({
+                title,
+                media,
+                bids,
+                endsAt,
+                id
+            }));
+
+            if (auctionData.length > 0) {
+                auctionData.forEach(item => {
+                    const norwegianTime = norwegianEndDate(item.endsAt);
+                    auctionContainer.append(articleCard(item.title, item.media, item.bids, norwegianTime, item.id));
+                });
+            } else {
+                auctionContainer.innerHTML = "No data available.";
+                buttonContainer.display = "none"
+
+            }
+        }
+    } finally {
+
     }
-    nextButton.addEventListener("click", async (e) => {
-        e.target.href = `/bidding.html?page=${getPageNum + 1}`
-    })
-
-    prevButton.addEventListener("click", async (e) => {
-        e.target.href = `/bidding.html?page=${getPageNum - 1}`
-    })
-
-}
+};
 
 
 
-
-function createCards(media) {
-    const article = createCardElement("article")
-    const articleHeader = createCardElement("div", "relative h-full w-full")
-    const articleImage = createCardElement("img")
-    articleImage.src = media;
-    articleImage.loading = "lazy"
-    articleHeader.append(articleImage)
-    const articleBody = createCardElement("div")
-    const articleFooter = createCardElement("div")
-    article.append(articleHeader)
-    return article
-}
-
-
-const renderListings = async () => {
-    let end = false;
-    const listingsContainer = document.querySelector("[data-type-section='listings']")
-    const listingData = await fetchListings()
-    console.log(listingData)
-    listingsContainer.innerHTML = "";
-
-    if (listingData.length === 0) {
-        listingsContainer.innerHTML = "No listings"
-        end = true;
-        listeners(end)
-
-    } else {
-        listingData.forEach(item => {
-            const { media } = item;
-            const cards = createCards(media)
-            listingsContainer.append(cards)
-
-        })
-    }
-
-}
+initializer();
 
 
 
-
-
-
-
-
-const initializer = () => {
-    fetchListings()
-    listeners()
-    renderListings()
-}
-initializer() */
