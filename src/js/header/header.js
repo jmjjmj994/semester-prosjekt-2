2f109af7f2c7eaa9956276f7a54f306934a06ac4
jmjjmj994 <jonathan.mjos@outlook.com>
2023-12-12 11:42:24 +0100

    Header usercontainer will change style based on content, instead of keeping a hardcoded style

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -2,7 +2,7 @@ import { singleProfile } from "../api/api.js";
 import { localStorageItems } from "../utils/utils.js";
 const navbar = document.querySelector("[data-type-component='navbar']")
 const header = document.querySelector("header")
- const navbarListeners = () => {
+const navbarListeners = () => {
     const hamburgerBtn = document.querySelector('[data-header="hamburger-menu"]');
     const closeNavbar = document.querySelector("[data-type-navbar='close-menu']")
     hamburgerBtn.onclick = () => navbarToggler(true);
@@ -10,7 +10,7 @@ const header = document.querySelector("header")
 }
 
 const navbarToggler = (value) => {
-    if(value) {
+    if (value) {
         navbar.classList.add("isActive")
     } else {
         navbar.classList.remove("isActive")
@@ -19,14 +19,16 @@ const navbarToggler = (value) => {
 
 
 
-const userAvatar =  async () => {
+const userAvatar = async () => {
+    const userContainer = document.querySelector("[data-type-navbar='user-container']")
     const profileImage = document.querySelectorAll("[data-type-user='avatar']")
     if (localStorageItems && localStorageItems.token) {
         const data = await singleProfile(localStorageItems.userData.name)
+        userContainer.className = "flex items-center   w-full   lg:min-w-[5rem] lg:w-[8rem]"
         profileImage.forEach(img => {
             img.src = data.avatar
         })
-    }  else {
+    } else {
         profileImage.forEach(img => {
             img.src = "src/assets/blank-avatar.png"
         })
