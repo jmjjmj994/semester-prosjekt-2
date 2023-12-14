abbd31c1eea748f8371af9b20e1dff24c9b92f36
jmjjmj994 <jonathan.mjos@outlook.com>
2023-12-14 22:42:59 +0100

    Created one function which is creating cards, can now remove uneccesary code

--- src/js/pages/bidding.js
+++ src/js/pages/bidding.js
@@ -1,4 +1,4 @@
-import { createCardElement, norwegianEndDate } from "../utils/utils.js"
+import { articleCard, createCardElement, norwegianEndDate } from "../utils/utils.js"
 const params = new URLSearchParams(window.location.search);
 const auctionContainer = document.querySelector("[data-type-section='listings']")
 const auctionHeader = document.querySelector("[data-type-section='result-information']")
@@ -11,9 +11,9 @@ const allBtn = document.querySelector("[data-type-bidding-sort='all']")
 const ascendingBtn = document.querySelector("[data-type-bidding-sort='asc']")
 const descendingBtn = document.querySelector("[data-type-bidding-sort='desc']")
 let getPageNum;
-ascendingBtn.addEventListener("click",(e)  => {
-   let url = `/bidding.html?order=asc`
-   window.location.href = url
+ascendingBtn.addEventListener("click", (e) => {
+    let url = `/bidding.html?order=asc`
+    window.location.href = url
 })
 descendingBtn.addEventListener("click", (e) => {
     let url = `/bidding.html?order=desc`
@@ -118,30 +118,30 @@ const initializer = async () => {
         handleData(data);
     }
 
-  if(ascendingOrder) {
+    if (ascendingOrder) {
         const asc = ascendingOrder.substring(" ")
         auctionHeader.textContent = `Sortert høyest`
-      nextButton.addEventListener("click", (e) => {
-          e.target.href = `/bidding.html?order=${asc}&page=${getPageNum + 1}`;
-      });
-      prevButton.addEventListener("click", (e) => {
-          e.target.href = `/bidding.html?order=${asc}&page=${getPageNum - 1}`;
-      });
-      const data = await sortListings(asc);
-      handleData(data)
+        nextButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?order=${asc}&page=${getPageNum + 1}`;
+        });
+        prevButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?order=${asc}&page=${getPageNum - 1}`;
+        });
+        const data = await sortListings(asc);
+        handleData(data)
     } else if (descendingOrder) {
-      const desc = descendingOrder.substring(" ")
-      auctionHeader.textContent = `Sortert lavest`
-      nextButton.addEventListener("click", (e) => {
-          e.target.href = `/bidding.html?order=${desc}&page=${getPageNum + 1}`;
-      });
-      prevButton.addEventListener("click", (e) => {
-          e.target.href = `/bidding.html?order=${desc}&page=${getPageNum - 1}`;
-      });
-      const data = await sortListings(desc);
-      handleData(data)
+        const desc = descendingOrder.substring(" ")
+        auctionHeader.textContent = `Sortert lavest`
+        nextButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?order=${desc}&page=${getPageNum + 1}`;
+        });
+        prevButton.addEventListener("click", (e) => {
+            e.target.href = `/bidding.html?order=${desc}&page=${getPageNum - 1}`;
+        });
+        const data = await sortListings(desc);
+        handleData(data)
     }
- 
+
 
 };
 
@@ -158,7 +158,7 @@ const handleData = (data) => {
     } else if (data.length < 10) {
         nextButton.classList.add('hidden');
         prevButton.classList.remove('hidden');
-       
+
     } else if (getPageNum === 0) {
         nextButton.classList.remove("hidden");
         prevButton.classList.add("hidden");
@@ -167,8 +167,8 @@ const handleData = (data) => {
         prevButton.classList.remove("hidden");
     }
 
-renderAuctionCards(data)
-    
+    renderAuctionCards(data)
+
 
 };
 
@@ -178,62 +178,29 @@ renderAuctionCards(data)
 
 
 
-
-const auctionCards = async (title, image, bids, date, id) => {
-
-     const createBidArray = bids.map(bid => bid.amount)
-    const highestBid =  createBidArray.length > 0 ? Math.max(...createBidArray) : 0;
-    const totalBids = createBidArray.reduce((accumulator, bidObj) => accumulator + bidObj,0)  
-    const article = createCardElement("article", "flex flex-col  relative  bg-custom-card relative shadow-md text-custom-textGrey card-no-effect card-effect");
-    const articleLink = createCardElement("a", "absolute h-full w-full custom-z-low");
-    articleLink.href = `/specific.html?id=${id}`
-    const articleHeader = createCardElement("div", " h-[70%] relative")
-    const articleHeaderImage = createCardElement("img", "absolute object-cover w-full h-full ");
-    image.length === 0 ? articleHeaderImage.src = "src/assets/no-image.jpg" : articleHeaderImage.src = image;
-    articleHeaderImage.alt = "Auksjons-produkt";
-    articleHeader.append(articleHeaderImage)
-    const articleBody = createCardElement("div", "basis-[auto] flex   p-1 overflow-hidden");
-    const articleBodyTitle = createCardElement("span", "card-title-typography mt-6");
-    articleBodyTitle.textContent = title;
-    articleBody.append(articleBodyTitle)
-    const articleFooter = createCardElement("div", "flex basis-[auto] py-3 justify-between items-end p-1 ");
-    const articleFooterCol1 = createCardElement("div", "flex flex-col ")
-    const articleFooterCol1TotalBids = createCardElement("span", "mb-3")
-    articleFooterCol1TotalBids.textContent = `Totale bud:${totalBids}` 
-    const articleFooterCol1HighestBid = createCardElement("span", "")
-    articleFooterCol1HighestBid.textContent = `Høyeste bud:${highestBid}`
-    articleFooterCol1.append(articleFooterCol1TotalBids, articleFooterCol1HighestBid)
-    const articleFooterCol2 = createCardElement("div", "");
-    const articleFooterCol2Icon = createCardElement("i", "fa-regular fa-clock")
-    const articleFooterCol2EndDate = createCardElement("span")
-    articleFooterCol2EndDate.textContent = date
-    articleFooterCol2.append(articleFooterCol2Icon, articleFooterCol2EndDate)
-    articleFooter.append(articleFooterCol1, articleFooterCol2)
-    article.append(articleLink)
-    article.append(articleHeader, articleBody, articleFooter) 
-    return article;
-}
-
-
 const renderAuctionCards = async (data) => {
 
+
     try {
-   
-        if(typeof data === "string" || data.length === 0) {
+
+        if (typeof data === "string" || data.length === 0) {
             auctionContainer.innerHTML = `<span> Ingen data tilgjengelig. Trykk <a class="text-purple-600 underline" href="/bidding.html">her</a> for å gå tilbake. </span>`;
         } else {
             auctionContainer.innerHTML = "";
             const cardPromises = data.map(async (item) => {
-                const { title, media, bids, endsAt, id } = item;
-                const norwegianTime = norwegianEndDate(endsAt);
-                return auctionCards(title, media, bids, norwegianTime, id);
+                const { media, title, bids, endsAt, id } = item;
+                const createBidArray = bids.map(bid => bid.amount)
+                const highestBid = createBidArray.length > 0 ? Math.max(...createBidArray) : 0;
+                const card = articleCard(media, title, highestBid, endsAt, id);
+                return card
+
             });
 
             const cards = await Promise.all(cardPromises);
             cards.forEach(card => auctionContainer.appendChild(card));
         }
-        
-    } finally{}
+
+    } finally { }
 }
 
 
@@ -241,3 +208,5 @@ const renderAuctionCards = async (data) => {
 
 
 initializer();
+
+
--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,9 +1,10 @@
-export { localStorageItems, createCardElement, createButtonElement, blockElements,dateConverter}
+export { localStorageItems, createCardElement, createButtonElement, blockElements, dateConverter }
 const localStorageItems = {
     userData: JSON.parse(localStorage.getItem("user-data")),
     token: localStorage.getItem("user-token"),
     hasToken: localStorage.getItem("tokenChecked"),
-    media: localStorage.getItem("new-media")}
+    media: localStorage.getItem("new-media")
+}
 
 
 
@@ -34,13 +35,14 @@ const createButtonElement = (className) => {
 const dateConverter = (date) => {
 
     const options = {
-        weekday:"long",
-        year:"numeric", 
-        month:"long",
-        day:"numeric",
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
         hour: "numeric",
         minute: "numeric",
-        second: "numeric",}
+        second: "numeric",
+    }
 
     const norwegianDate = new Date(date).toLocaleString("no-NO", options)
 
@@ -62,3 +64,62 @@ export const norwegianEndDate = (endDate) => {
     return `${days}d, ${hours}t, ${minutes}m`;
 }
 
+
+
+
+
+
+
+const cardHeader = (image) => {
+    const cardHeader = createCardElement("div", "h-[70%] relative") /**Header */
+    const cardHeaderImage = createCardElement("img", "absolute object-cover w-full h-full ")
+    image.length === 0 ? cardHeaderImage.src = "src/assets/no-image.jpg" : cardHeaderImage.src = image;
+  
+    cardHeaderImage.alt = "Image of product"
+    cardHeader.append(cardHeaderImage)
+    return cardHeader;
+}
+
+const cardBody = (title) => {
+    const cardBody = createCardElement("div", "basis-[auto] flex   p-1 overflow-hidden")
+    const cardBodyTitle = createCardElement("p", "card-title-typography")
+    cardBodyTitle.textContent = title;
+    cardBody.append(cardBodyTitle)
+    return cardBody
+}
+
+const cardFooter = (bids, endsAt) => {
+    const norwegianFormattedDate = norwegianEndDate(endsAt)
+    const cardFooter = createCardElement("div", "flex basis-[auto]  justify-between items-end p-1 ")
+    const cardFooterCol_1 = createCardElement("div", "");
+    const cardFooterBids = createCardElement("p", "")
+    cardFooterBids.textContent = `Høyeste bud: ${bids}`
+    cardFooterCol_1.append(cardFooterBids)
+   
+    const cardFooterCol_2 = createCardElement("div", "flex gap-1 px-1 shadow-xl bg-custom-background")
+    const cardFooterCol_2Icon = createCardElement("span", "")
+    cardFooterCol_2Icon.innerHTML = `<i class="fa-regular fa-clock"></i>`
+    const cardFooterDate = createCardElement("p", "")
+    cardFooterDate.textContent = `${norwegianFormattedDate}`
+    cardFooterCol_2.append(cardFooterCol_2Icon, cardFooterDate)
+    cardFooter.append(cardFooterCol_1, cardFooterCol_2);
+    return cardFooter
+
+}
+
+
+
+
+
+
+export const articleCard = (image, title, bids, endsAt,id) => {
+    const article = createCardElement("article", "flex flex-col justify-between relative  bg-custom-card relative shadow-md text-custom-textGrey card-no-effect card-effect relative");
+    const link = createCardElement("a", "inset-0 absolute  custom-z-low");
+    link.href = `/specific.html?id=${id}`
+    const articleHeader = cardHeader(image)
+    const articleBody = cardBody(title)
+    const articleFooter = cardFooter(bids, endsAt)
+    article.append(link,articleHeader, articleBody, articleFooter)
+    return article
+
+}
